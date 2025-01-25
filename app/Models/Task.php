ab7b1f42de28f93f76589bf84cc8cd02f10e61d2
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-01-25 10:25:10 +0100

    Task Model

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+
+    /**
+     * The users that belong to the task.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
