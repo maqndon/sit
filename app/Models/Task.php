6c0f18db63da32a74fc779b9a36aa50b5470d24d
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-02-01 15:34:15 +0100

    elequent relations updated

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -25,4 +25,9 @@ class Task extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
