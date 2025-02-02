4c6ce94a6d9fef8d7a803d61231bc105603816ab
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-02-02 17:21:54 +0100

    refactored

--- tests/Traits/UserTrait.php
+++ tests/Traits/UserTrait.php
@@ -2,30 +2,82 @@
 
 namespace Tests\Traits;
 
+use App\Models\Project;
 use App\Models\Task;
 use App\Models\User;
 
 trait UserTrait
 {
-    protected $user;
+    protected ?User $user = null;
 
-    protected $task;
+    protected ?Project $project = null;
 
-    public function createUser(): User
+    protected ?Task $task = null;
+
+    /**
+     * Create a regular user.
+     */
+    public function createUser(array $attributes = []): User
     {
-        return $this->user = User::factory()->create();
+        return $this->user = User::factory()->create($attributes);
     }
 
+    /**
+     * Create an admin user.
+     */
     public function createAdminUser(): User
     {
-        return $this->user = User::factory()->create(['role' => 'admin']);
+        return $this->createUser(['role' => 'admin']);
     }
 
+    /**
+     * Create a project associated with a user.
+     */
+    public function createProject(array $attributes = []): Project
+    {
+        return $this->project = Project::factory()->create($attributes);
+    }
+
+    /**
+     * Create a task associated with a user or project.
+     */
+    public function createTask(array $attributes = []): Task
+    {
+        return $this->task = Task::factory()->create($attributes);
+    }
+
+    /**
+     * Create a user with an associated task.
+     */
     public function createUserWithTask(): User
     {
-        $this->user = User::factory()->create();
-        Task::factory()->create(['user_id' => $this->user->id]);
+        $user = $this->createUser();
+        $this->createTask(['user_id' => $user->id]);
 
-        return $this->user;
+        return $user;
+    }
+
+    /**
+     * Create a user with an associated project.
+     */
+    public function createUserWithProject(): User
+    {
+        $user = $this->createUser();
+        $this->createProject(['user_id' => $user->id]);
+
+        return $user;
+    }
+
+    /**
+     * Create a user with a project and a specified number of tasks.
+     */
+    public function createUserWithProjectAndTasks(int $taskCount = 1): User
+    {
+        $user = $this->createUser();
+        $project = $this->createProject(['user_id' => $user->id]);
+
+        Task::factory($taskCount)->create(['user_id' => $user->id, 'project_id' => $project->id]);
+
+        return $user;
     }
 }
