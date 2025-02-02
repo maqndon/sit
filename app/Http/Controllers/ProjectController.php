732cb10ef7b25b7aa1dd5766dd3e42c47aeb5768
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-02-02 17:22:47 +0100

    project controller

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Projects\StoreProjectRequest;
+use App\Http\Requests\Projects\UpdateProjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use App\Services\ProjectService;
+use Illuminate\Http\JsonResponse;
+
+class ProjectController extends Controller
+{
+    protected $projectService;
+
+    public function __construct(ProjectService $projectService)
+    {
+        $this->projectService = $projectService;
+    }
+
+    /**
+     * Display a listing of projects.
+     */
+    public function index(): JsonResponse
+    {
+        $projects = $this->projectService->getProjects();
+
+        return response()->json(ProjectResource::collection($projects));
+    }
+
+    /**
+     * Store a newly created project.
+     */
+    public function store(StoreProjectRequest $request): JsonResponse
+    {
+        $project = $this->projectService->store($request);
+
+        return response()->json(new ProjectResource($project), 201);
+    }
+
+    /**
+     * Display the specified project.
+     */
+    public function show(Project $project): JsonResponse
+    {
+        $this->projectService->authorizeProject($project);  // Check if the user is authorized
+
+        return response()->json(new ProjectResource($project->load('tasks')));
+    }
+
+    /**
+     * Update the specified project.
+     */
+    public function update(UpdateProjectRequest $request, Project $project): JsonResponse
+    {
+        $this->projectService->authorizeProject($project);
+
+        $project = $this->projectService->update($request, $project);
+
+        return response()->json(new ProjectResource($project));
+    }
+
+    /**
+     * Remove the specified project.
+     */
+    public function destroy(Project $project): JsonResponse
+    {
+        $this->projectService->delete($project);
+
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Get tasks by project.
+     */
+    public function getTasks(Project $project): JsonResponse
+    {
+        return response()->json($project->tasks);
+    }
+}
