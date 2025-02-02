74142f2df77d25fff8e082b9269b92f67b2ec74d
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-02-02 17:22:29 +0100

    some resources added

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $user = $request->user();
+
+        return [
+            'id' => $this->id,
+            'owner' => new UserSummaryResource($this->whenLoaded('user')),
+            'name' => $this->name,
+            'description' => $this->description,
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
