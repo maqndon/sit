f517850a304a48df580b6c447419e69d01a8377d
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2025-02-09 13:34:46 +0100

    reordered

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -18,11 +18,11 @@ class TaskResource extends JsonResource
 
         return [
             'title' => $this->title,
-            'project' => new ProjectSummaryResource($this->whenLoaded('project')),
             'description' => $this->description,
             'status' => $this->status,
             'deadline' => $this->deadline,
             'owner' => new UserSummaryResource($this->whenLoaded('user')),
+            'project' => new ProjectSummaryResource($this->whenLoaded('project')),
         ];
     }
 }
